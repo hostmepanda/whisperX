653a0d5ec5001bb5062ed91292cac26218b918d0
Slava <45483857+hostmepanda@users.noreply.github.com>
2025-12-15 21:27:11 +0200

    Comment out default align models for en, fr, de, es, it
    Commented out default models for English and French in the Torch align models.

--- whisperx/alignment.py
+++ whisperx/alignment.py
@@ -29,15 +29,20 @@ PUNKT_ABBREVIATIONS = ['dr', 'vs', 'mr', 'mrs', 'prof']
 LANGUAGES_WITHOUT_SPACES = ["ja", "zh"]
 
 DEFAULT_ALIGN_MODELS_TORCH = {
-    "en": "WAV2VEC2_ASR_BASE_960H",
-    "fr": "VOXPOPULI_ASR_BASE_10K_FR",
+    # "en": "WAV2VEC2_ASR_BASE_960H",
+    # "fr": "VOXPOPULI_ASR_BASE_10K_FR",
     # "de": "VOXPOPULI_ASR_BASE_10K_DE",
-    "es": "VOXPOPULI_ASR_BASE_10K_ES",
-    "it": "VOXPOPULI_ASR_BASE_10K_IT",
+    # "es": "VOXPOPULI_ASR_BASE_10K_ES",
+    # "it": "VOXPOPULI_ASR_BASE_10K_IT",
 }
 
 DEFAULT_ALIGN_MODELS_HF = {
     "de": "jonatasgrosman/wav2vec2-large-xlsr-53-german",
+    "fr": "jonatasgrosman/wav2vec2-large-xlsr-53-french",
+    "es": "jonatasgrosman/wav2vec2-large-xlsr-53-spanish",
+    "it": "jonatasgrosman/wav2vec2-large-xlsr-53-italian",
+    "en": "jonatasgrosman/wav2vec2-large-xlsr-53-english",
+    
     "ja": "jonatasgrosman/wav2vec2-large-xlsr-53-japanese",
     "zh": "jonatasgrosman/wav2vec2-large-xlsr-53-chinese-zh-cn",
     "nl": "jonatasgrosman/wav2vec2-large-xlsr-53-dutch",
